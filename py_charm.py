a4a4a632363603d9b56929c2672c614ab604a2c2
MokhammadK <karina.mokhammad@gmail.com>
2023-04-11 20:54:05 +0300

    add if to all inputs numeric

--- py_charm.py
+++ py_charm.py
@@ -1,5 +1,8 @@
 def py_charm_function(x, a, b, c):
+    if not isinstance(x, (int, float)) or not isinstance(a, (int, float)) or not isinstance(b, (int, float)) or not isinstance(c, (int, float)):
+        raise TypeError("Error, all inputs must be numeric")
     if a == 0:
         raise ValueError("Error, 'a' cannot be 0")
     y = a*(x**2) + b*x + c
     return y
+
