740ff81b2a5f20d310b37148b14f303ce4b25a0f
MokhammadK <karina.mokhammad@gmail.com>
2023-04-11 20:58:41 +0300

    add new function love_or_tears

--- py_charm.py
+++ py_charm.py
@@ -3,3 +3,8 @@ def py_charm_function(x, a, b, c):
         raise ValueError("Error, 'a' cannot be 0")
     y = a*(x**2) + b*x + c
     return y
+
+def love_or_tears(word):
+    num_letters = len(word)
+    if num_letters % 2 == 0:
+        return "Love"
